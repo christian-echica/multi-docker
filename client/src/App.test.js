a7b27603d40a95d8d04ca7bcdda7f45b4933f822
echica.christian <echica.christian@gmail.com>
2020-06-30 17:28:58 +0800

    app test modified

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {});
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App component', () => {
+    it('starts with a count of 0', () => {
+        const wrapper = shallow(<App />);
+        const text = wrapper.find('p').text();
+        expect(text).toEqual('Count: 0');
+    });
+});
