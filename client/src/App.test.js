343b045cad6867e1b09ea1f70a58477795251a90
echica.christian <echica.christian@gmail.com>
2020-06-30 17:34:29 +0800

    jst enjoying

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -6,13 +6,4 @@ import App from './App';
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
-
-describe('App component', () => {
-    it('starts with a count of 0', () => {
-        const wrapper = shallow(<App />);
-        const text = wrapper.find('p').text();
-        expect(text).toEqual('Count: 0');
-    });
 });
